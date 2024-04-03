1b247d02671cc7932f031b76c65736c811defaf9
StaRafael <rafael_silv@hotmail.com>
2024-04-03 19:12:41 -0300

    vetor e Matriz

---
+++ Listas/Lista 6/Exercicio 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    const int QAUNTIDADE_PESSOAS = 10;
+    string[] nome = new string[QAUNTIDADE_PESSOAS];
+    bool resultado = false;
+
+    for (int linha = 0; linha < QAUNTIDADE_PESSOAS; linha++)
+    {
+      nome[linha] = Console.ReadLine();
+    }
+
+    string pesquisa = Console.ReadLine();
+
+    for (int linha = 0; linha < QAUNTIDADE_PESSOAS; linha++)
+    {
+      if (pesquisa == nome[linha])
+      {
+        resultado = true;
+      }
+    }
+
+    if (resultado == true)
+    {
+      Console.WriteLine("ACHEI");
+    }
+    else
+    {
+      Console.WriteLine("NAO ACHEI");
+    }
+
+  }
+}
---
+++ Listas/Lista 6/Exercicio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    const int QUANTIDADE_DE_NOME = 10;
+    string[] nome = new string[QUANTIDADE_DE_NOME];
+
+    for (int linha = 0; linha < QUANTIDADE_DE_NOME; linha++)
+    {
+      nome[linha] = Console.ReadLine();
+    }
+
+    for (int linha = QUANTIDADE_DE_NOME - 1; linha >= 0; linha--)
+    {
+      if (linha % 2 == 0)
+      {
+        Console.WriteLine(nome[linha]);
+      }
+    }
+  }
+}
